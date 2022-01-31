5ed46959318e0a3c4c3cc94372e0f0d0aa346655
Gabriel Prates <gabriel.prates@trust.group>
2022-01-31 12:57:14 -0300

    refatoração dos serviços do módulo de clientes, aplicar pattern SOLID, inversão de dependências

--- .history/src/modules/customers/services/CreateCustomerService_20220131125310.ts
+++ .history/src/modules/customers/services/CreateCustomerService_20220131125310.ts
@@ -4,7 +4,9 @@ import { ICustomer } from '../domain/models/ICustomer';
 import { ICustomersRepository } from '../domain/repositories/ICustomersRepository';
 
 class CreateCustomerService {
-    constructor(private customersRepository: ICustomersRepository) {}
+    constructor(private customersRepository: ICustomersRepository) {
+
+    }
 
     public async execute({ name, email }: ICreateCustomer): Promise<ICustomer> {
         const emailExists = await this.customersRepository.findByEmail(email);
